f99470f62bb1b8fc15db59a955c773e8f32895e4
thenikolayli <nikolay.li2008@gmail.com>
2025-04-01 22:46:17 -0700

    aaa

--- frontend/src/components/Carousel.tsx
+++ frontend/src/components/Carousel.tsx
@@ -7,23 +7,28 @@ const Carousel = () => {
     // const item_width = 100
 
     onMount(() => {
-        const gallery_container_left = document.querySelector(".gallery-container")!.getBoundingClientRect().left
-        const gallery_container_right = document.querySelector(".gallery-container")!.getBoundingClientRect().right
+        const gallery_container_left = document.querySelector(".gallery-inner-container")!.getBoundingClientRect().left
+        const left_bound = gallery_container_left - 100 // when the element is fully outside of the inner container
+        const right_bound = document.querySelector(".gallery-inner-container")!.getBoundingClientRect().right
 
         Draggable.create(".gallery-item"!, {
             type: "x",
             bounds: ".gallery-container"!,
-            onDrag: (event) => {
-                const object_left = event.target.getBoundingClientRect().left
-                const object_right = event.target.getBoundingClientRect().right
-                console.log(object_left - gallery_container_left)
+            onDrag: (self) => {
+                let element_left = self.target.getBoundingClientRect().left;
+                let element_x = element_left;
+
+                console.log(element_x, left_bound, right_bound);
+                gsap.set(self.target, {x: element_x})
             }
         })
     })
 
     return (
-        <div class={"gallery-container relative w-[300px] h-[100px] border-black border-2 mx-auto"}>
-            <div class={`absolute gallery-item w-[100px] h-[100px] bg-black/50`}></div>
+        <div class={"gallery-container relative w-[600px] h-[100px] border-black border-2 mx-auto"}>
+            <div class={"gallery-inner-container relative w-[300px] h-[100px] border-black border-2 mx-auto"}>
+                <div class={`absolute gallery-item w-[100px] h-[100px] bg-black/50`}></div>
+            </div>
         </div>
     )
 }
